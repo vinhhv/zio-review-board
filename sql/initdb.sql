c6e30973084a1a745bf3fa4707901baaea503c6f
vinhhv <vinhhv@users.noreply.github.com>
2024-02-26 22:54:39 +0000

    Add trainers table

--- sql/initdb.sql
+++ sql/initdb.sql
@@ -1,6 +1,16 @@
 CREATE DATABASE reviewboard;
 \c reviewboard;
 
+-- TRAINERS
+
+CREATE TABLE IF NOT EXISTS trainers (
+  id BIGSERIAL PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL,
+  description TEXT NOT NULL,
+  url TEXT UNIQUE NOT NULL,
+  image TEXT
+);
+
 -- PROGRAMS
 
 CREATE TYPE payment_type AS ENUM('LifetimeAccess', 'Subscription', 'SubscriptionOrLifetimeAccess');
@@ -10,7 +20,7 @@ CREATE TABLE IF NOT EXISTS programs (
   slug TEXT UNIQUE NOT NULL,
   name TEXT UNIQUE NOT NULL,
   url TEXT UNIQUE NOT NULL,
-  trainer TEXT NOT NULL,
+  trainer_id BIGSERIAL NOT NULL REFERENCES trainers(id),
   payment_type payment_type NOT NULL,
   image TEXT,
   tags TEXT[]
@@ -21,19 +31,19 @@ CREATE TABLE IF NOT EXISTS programs (
 CREATE TYPE metric_score AS ENUM('Poor', 'Fair', 'Good', 'Great', 'Amazing');
 
 CREATE TABLE IF NOT EXISTS reviews (
-    id BIGSERIAL PRIMARY KEY,
-    program_id BIGINT NOT NULL,
-    user_id BIGINT NOT NULL,
-    value metric_score NOT NULL,
-    quality metric_score NOT NULL,
-    content metric_score NOT NULL,
-    user_experience metric_score NOT NULL,
-    accessibility metric_score NOT NULL,
-    support metric_score NOT NULL,
-    would_recommend metric_score NOT NULL,
-    review TEXT NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT now(),
-    updated TIMESTAMP NOT NULL DEFAULT now()
+  id BIGSERIAL PRIMARY KEY,
+  program_id BIGSERIAL NOT NULL REFERENCES programs(id),
+  user_id BIGINT NOT NULL,
+  value metric_score NOT NULL,
+  quality metric_score NOT NULL,
+  content metric_score NOT NULL,
+  user_experience metric_score NOT NULL,
+  accessibility metric_score NOT NULL,
+  support metric_score NOT NULL,
+  would_recommend metric_score NOT NULL,
+  review TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT now(),
+  updated TIMESTAMP NOT NULL DEFAULT now()
 );
 
 CREATE OR REPLACE FUNCTION update_modified_column()
